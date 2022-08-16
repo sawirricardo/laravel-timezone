761fee60c0c30c40f0c4466065ba10e3a56a1ecf
sawirricardo <sawir.ricardo@gmail.com>
2022-08-16 13:35:23 +0700

    add bootingPackage

--- src/LaravelTimezoneServiceProvider.php
+++ src/LaravelTimezoneServiceProvider.php
@@ -2,8 +2,11 @@
 
 namespace Sawirricardo\LaravelTimezone;
 
+use Illuminate\Support\Facades\Blade;
+use Illuminate\Support\Facades\Route;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
+use Sawirricardo\LaravelTimezone\Components\LaravelTimezoneComponent;
 
 class LaravelTimezoneServiceProvider extends PackageServiceProvider
 {
@@ -13,5 +16,14 @@ class LaravelTimezoneServiceProvider extends PackageServiceProvider
             ->name('laravel-timezone')
             ->hasConfigFile()
             ->hasViews();
+
+        Blade::component(LaravelTimezoneComponent::class, 'laravel-timezone');
+    }
+
+    public function bootingPackage()
+    {
+        Route::post(config('timezone.url'), config('timezone.controller'))
+            ->name('laravel-timezone')
+            ->middleware(config('timezone.middleware'));
     }
 }
