46a289acbe00ba22a53bcea5b323f4a0dc20d69c
sawirricardo <sawir.ricardo@gmail.com>
2022-08-16 13:31:02 +0700

    Update timezone.php

--- config/timezone.php
+++ config/timezone.php
@@ -1,8 +1,15 @@
 <?php
 
-// config for Sawirricardo/LaravelTimezone
 return [
-    'request' => [
-        'header' => 'X-Timezone',
+    'url' => 'laravel-timezone',
+    'controller' => \Sawirricardo\LaravelTimezone\Http\Controllers\LaravelTimezoneController::class,
+
+    'allowed_timezones' => [
+        ...timezone_identifiers_list(),
     ],
+
+    'middleware' => [
+        'web',
+    ],
+
 ];
