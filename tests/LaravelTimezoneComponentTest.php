6a2d8e1a8e2ef4390870f3446cbafd878429e301
sawirricardo <sawir.ricardo@gmail.com>
2022-08-16 13:35:40 +0700

    add tests

---
+++ tests/LaravelTimezoneComponentTest.php
@@ -0,0 +1,11 @@
+<?php
+
+use Illuminate\Support\Facades\Blade;
+
+use function Spatie\Snapshots\assertMatchesHtmlSnapshot;
+
+it('renders the component', function () {
+    $html = Blade::render('<x-laravel-timezone />');
+
+    assertMatchesHtmlSnapshot($html);
+});
---
+++ tests/LaravelTimezoneControllerTest.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Support\Facades\Cache;
+
+use function Pest\Laravel\postJson;
+
+it('stores the timezone into the session', function () {
+    postJson(route('laravel-timezone'), ['timezone' => 'Europe/Berlin'])
+        ->assertCreated();
+    expect(currentTimezone())->toBe('Europe/Berlin');
+});
+
+it('throws validation errors when the timezone is not valid', function () {
+    postJson(route('laravel-timezone'), ['timezone' => 'invalid'])
+        ->assertUnprocessable();
+    expect(currentTimezone())->toBeString(config('app.timezone'));
+});
