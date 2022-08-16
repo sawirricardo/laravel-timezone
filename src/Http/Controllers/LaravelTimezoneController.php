7030c0f542fef72e3226a27e38f840897fdb8ff7
sawirricardo <sawir.ricardo@gmail.com>
2022-08-16 16:09:28 +0700

    fire events before and after timezone update

--- src/Http/Controllers/LaravelTimezoneController.php
+++ src/Http/Controllers/LaravelTimezoneController.php
@@ -2,6 +2,8 @@
 
 namespace Sawirricardo\LaravelTimezone\Http\Controllers;
 
+use Sawirricardo\LaravelTimezone\Events\TimezoneUpdated;
+use Sawirricardo\LaravelTimezone\Events\UpdatingTimezone;
 use Sawirricardo\LaravelTimezone\Requests\LaravelTimezoneRequest;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -9,16 +11,15 @@ class LaravelTimezoneController
 {
     public function __invoke(LaravelTimezoneRequest $request)
     {
+        event(new UpdatingTimezone($request->timezone, clientIp()));
+
         if (config('timezone.driver') === 'session') {
             session()->put($this->getKey(), $request->timezone);
-
-            return response()->noContent(Response::HTTP_CREATED);
+        } else {
+            cache()->rememberForever($this->getKey(), fn () => $request->timezone);
         }
 
-        cache()->rememberForever(
-            $this->getKey(),
-            fn () => $request->timezone
-        );
+        event(new TimezoneUpdated($request->timezone, clientIp()));
 
         return response()->noContent(Response::HTTP_CREATED);
     }
